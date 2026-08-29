cef6eca593e643b41b69110d24331e1a66854569
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-08-29 19:51:33 +0200

    Fix: AGP 8.5.2 + Gradle 8.7 + disable viewBinding - fix dataBindingMerge error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     buildTypes { release { isMinifyEnabled = false }; debug { isMinifyEnabled = false } }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_1_8; targetCompatibility = JavaVersion.VERSION_1_8 }
     kotlinOptions { jvmTarget = "1.8" }
-    buildFeatures { viewBinding = true }
+    buildFeatures { viewBinding = false; dataBinding = false }
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
