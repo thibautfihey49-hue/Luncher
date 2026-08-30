1e60d24c3e70bf38223a27c08c3dba1de68617b4
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-08-30 23:58:51 +0200

    V45.1 fix import View

--- app/src/main/java/com/luncher/PhoneAppActivity.kt
+++ app/src/main/java/com/luncher/PhoneAppActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
