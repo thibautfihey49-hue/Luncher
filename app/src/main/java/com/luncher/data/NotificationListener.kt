0ec271280c05c67acd19a5ab5bcdbc18f3a9c673
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-08-30 11:42:07 +0200

    FIX auto-start popup service

--- app/src/main/java/com/luncher/data/NotificationListener.kt
+++ app/src/main/java/com/luncher/data/NotificationListener.kt
@@ -4,7 +4,7 @@ import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 class NotificationListener : NotificationListenerService() {
     companion object{ private var instance: NotificationListener?=null; fun getInstance()=instance }
-    override fun onCreate(){ super.onCreate(); instance=this }
+    override fun onCreate(){ super.onCreate(); instance=this; FloatingNotificationService.show(this) }
     override fun onDestroy(){ instance=null; super.onDestroy() }
     override fun onNotificationPosted(sbn: StatusBarNotification){
         try{
@@ -12,8 +12,10 @@ class NotificationListener : NotificationListenerService() {
             val pm=packageManager
             val appName=try{pm.getApplicationLabel(pm.getApplicationInfo(sbn.packageName,0)).toString()}catch(_:Exception){sbn.packageName}
             NotificationRepository.add(sbn,appName)
+            FloatingNotificationService.show(this)
+            FloatingNotificationService.forceRefresh()
         }catch(_:Exception){}
     }
-    override fun onNotificationRemoved(sbn: StatusBarNotification){ try{NotificationRepository.remove(sbn.key)}catch(_:Exception){} }
+    override fun onNotificationRemoved(sbn: StatusBarNotification){ try{NotificationRepository.remove(sbn.key); FloatingNotificationService.forceRefresh()}catch(_:Exception){} }
     fun cancelNotif(key: String){ try{cancelNotification(key)}catch(_:Exception){} }
 }
